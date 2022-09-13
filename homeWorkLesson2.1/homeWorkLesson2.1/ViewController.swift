3e9fe47730bdf154ff9490055b6ac3a921d53be4
Kazakov Danil <kazakov@MacBook-Pro-Kazakov.local>
2022-09-13 18:40:13 +0300

    First commit

---
+++ homeWorkLesson2.1/homeWorkLesson2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWorkLesson2.1
+//
+//  Created by Kazakov Danil on 12.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
